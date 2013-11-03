5333fa12cfe8235bb68bbc073ce5bc5c3fa16b16
Phil Monroe <phil@philmonroe.com>
2013-11-03 09:31:37 -0800

    ability to create albums

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -3,4 +3,15 @@ class AlbumsController < ApplicationController
     @albums = Album.all
   end
 
+  def create
+    album = Album.create album_params
+    redirect_to album_photos_path(album)
+  end
+
+
+  private
+  def album_params
+    params.require(:album).permit(:name)
+  end
+
 end
